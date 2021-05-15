69c487e8491ce8bd827b9dbd350a7c6bfa53f04a
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-15 17:18:59 +0100

    Changed Navbar buttons to links with routes

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,36 +5,25 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
+        <Link to="/" className="navbar-brand">
           Spicy Mango
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        </Link>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Posts <span class="sr-only"></span>
-              </a>
+              <Link to="/" class="nav-link">
+                Posts
+              </Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <Link to="/create" class="nav-link">
                 Create Post
-              </a>
+              </Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <Link to="/user" class="nav-link">
                 Create User
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
