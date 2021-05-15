9aad7853cf6bf9c2fd30823c7b23beb497936e13
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-15 17:26:01 +0100

    Added components for all the links on the navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar.component.js";
+import PostsList from "./components/postsList.component";
+import EditPost from "./components/editPost.component";
+import CreatePost from "./components/createPost.component";
+import CreateUser from "./components/createUser.component";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
+      <Route path="/" exact component={PostsList} />
+      <Route path="/edit/:id" component={EditPost} />
+      <Route path="/create" component={CreatePost} />
+      <Route path="/user" component={CreateUser} />
     </Router>
   );
 }
---
+++ src/components/postsList.component.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class PostsList extends Component {
+  render() {
+    return <div>You are on the Posts List Component</div>;
+  }
+}
